d2890de2a4ef1e7ddefca470855cca3bd97cc48a
starplanter93 <starplanter93@gmail.com>
2023-10-17 20:48:11 +0900

    ES module

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,5 +1,5 @@
-// const moduleData = require('./math');
-const { add, sub } = require('./math');
+import mul, { add, sub } from './math.js';
 
 console.log(add(1, 2)); //3
 console.log(sub(1, 2)); //-1
+console.log(mul(10, 20));
--- section1/src/math.js
+++ section1/src/math.js
@@ -1,12 +1,11 @@
-function add(a, b) {
+export function add(a, b) {
   return a + b;
 }
 
-function sub(a, b) {
+export function sub(a, b) {
   return a - b;
 }
 
-module.exports = {
-  add,
-  sub,
-};
+export default function multiply(a, b) {
+  return a * b;
+}
