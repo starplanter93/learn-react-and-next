9b76f461bdd6bea241ace217862bbb3d2b5221a5
starplanter93 <starplanter93@gmail.com>
2023-10-19 20:54:03 +0900

    node 라이브러리 사용해보기

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,5 +1,6 @@
 import mul, { add, sub } from './math.js';
+import randomColor from 'randomcolor';
 
-console.log(add(1, 2)); //3
-console.log(sub(1, 2)); //-1
-console.log(mul(10, 20));
+const color = randomColor();
+
+console.log(color);
