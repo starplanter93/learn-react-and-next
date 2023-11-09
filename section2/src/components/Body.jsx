404793666df6f6a42ef52591a8935095db682ca4
starplanter93 <starplanter93@gmail.com>
2023-11-09 21:46:19 +0900

    state and props

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react';
 import './Body.css';
 
+function Lightbulb({ light }) {
+  return (
+    <>
+      {light === 'on' ? (
+        <div style={{ backgroundColor: 'orange' }}>on</div>
+      ) : (
+        <div style={{ backgroundColor: 'gray' }}>off</div>
+      )}
+    </>
+  );
+}
+
+function StaticLightbulb() {
+  console.log('static light bulb');
+  return <div style={{ backgroundColor: 'gray' }}>off</div>;
+}
+
 export default function Body() {
   const [light, setLight] = useState('off');
-  console.log(light);
 
   return (
     <div className='body'>
-      {light}
+      <Lightbulb light={light} />
+      <StaticLightbulb />
       <button
         onClick={() => {
           setLight('on');
