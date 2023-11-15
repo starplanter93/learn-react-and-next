0ad17f0a9d0fe8741c7b5e7518b9bd030865dfaa
starplanter93 <starplanter93@gmail.com>
2023-11-15 20:39:54 +0900

    카운트 기능

--- section4/src/components/Viewer.jsx
+++ section4/src/components/Viewer.jsx
@@ -1,8 +1,8 @@
-export default function Viewer() {
+export default function Viewer({ count }) {
   return (
     <div>
       <div>현재 카운트 :</div>
-      <h1>0</h1>
+      <h1>{count}</h1>
     </div>
   );
 }
