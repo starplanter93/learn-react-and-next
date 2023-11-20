b8222b96a7260ec51f3314c958c04fd92486cafe
starplanter93 <starplanter93@gmail.com>
2023-11-20 20:58:40 +0900

    mockdata

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
 
+const mockData = [
+  {
+    id: 0,
+    isDone: true,
+    content: 'react 공부하기',
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className='App'>
       <Header />
