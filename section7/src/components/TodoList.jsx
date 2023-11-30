c98c6b0f8944624335bf5b69553bce6113af41ed
starplanter93 <starplanter93@gmail.com>
2023-11-30 21:12:16 +0900

    useMemo

--- section7/src/components/TodoList.jsx
+++ section7/src/components/TodoList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import TodoItem from './TodoItem';
 import './TodoList.css';
 
@@ -19,9 +19,25 @@ export default function TodoList({ todos, onUpdate, onDelete }) {
     );
   };
 
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className='TodoList'>
       <h4>Todos</h4>
+      <div>
+        <div>전체 투두 : {totalCount}</div>
+        <div>완료 투두 : {doneCount}</div>
+        <div>미완 투두 : {notDoneCount}</div>
+      </div>
       <input placeholder='검색어를 입력하세요' onChange={onChangeSearch} />
       <div className='todos_wrapper'>
         {filterTodos().map((todo) => (
