72d57b013e110cd83fe6ca1c3147d1f9cbabdb29
starplanter93 <starplanter93@gmail.com>
2024-01-03 21:02:51 +0900

    page 이동

--- section12/pages/index.js
+++ section12/pages/index.js
@@ -1,3 +1,26 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Home() {
-  return <div>HomePage</div>;
+  const code = 'KOR';
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push('/search');
+  };
+  return (
+    <div>
+      HomePage
+      <div>
+        <button onClick={onClickButton}>search page 이동</button>
+      </div>
+      <div>
+        <Link href={'/search'}>search page 이동</Link>
+      </div>
+      <div>
+        <Link href={{ pathname: '/country/[code]', query: { code: code } }}>
+          {code} page 이동
+        </Link>
+      </div>
+    </div>
+  );
 }
